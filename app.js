96e2111668dd8fe916082d9485180e42dc89023d
Alaa Chahboune <89790591+achahboune@users.noreply.github.com>
2025-07-12 02:20:42 +0100

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const userRoutes = require('./routes/userRoutes');
 require('dotenv').config();
 
 const app = express();
+app.get('/', (req, res) => {
+  res.send('Backend AI Guardian API is running 🚀');
+});
+
 app.use(cors());
 app.use(express.json());
 app.use('/api/users', userRoutes);
