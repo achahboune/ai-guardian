4b8713806029cfdb702a9ede4b6cb08313ee2d3f
Alaa Chahboune <89790591+achahboune@users.noreply.github.com>
2025-07-12 02:39:26 +0100

    Update app.js

--- app.js
+++ app.js
@@ -5,15 +5,18 @@ const userRoutes = require('./routes/userRoutes');
 require('dotenv').config();
 
 const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/users', userRoutes);
+
+// Route home pour confirmer que le backend tourne
 app.get('/', (req, res) => {
   res.send('Backend AI Guardian API is running 🚀');
 });
 
-app.use(cors());
-app.use(express.json());
-app.use('/api/users', userRoutes);
-
-// MongoDB connection
+// Connexion MongoDB
 mongoose.connect(process.env.DB_URL)
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.error(err));
